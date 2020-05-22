6a40f0753da2dc9f9836539ce2e56f90a76bd4f0
Ashish <ashishkckse@gmail.com>
2020-05-22 20:38:20 +0530

    Added authentication token on user signup

--- substring_challenge_api/app/controllers/user/registrations_controller.rb
+++ substring_challenge_api/app/controllers/user/registrations_controller.rb
@@ -4,7 +4,9 @@ class User::RegistrationsController < Devise::RegistrationsController
   def create
     @user = User.new(sign_up_params)
     if @user.save
+      token = JsonWebToken.encode(user_id: @user.id)
       render json: { status: 200, email: @user.email,
+                     token: token,
                      message: 'Registration Successful!' }
     else
       render json: { errors: @user.errors.full_messages,
